0c42fb23397c7302fc2d489f9efd7c19169a956f
Kieran Scannell <kierscan@gmail.com>
2020-02-28 12:40:07 -0600

    Add login, logout, verifyUser mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,5 +1,5 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 const mongoose = require("mongoose");
 
 const AuthService = require("../services/auth");
@@ -20,6 +20,34 @@ const mutation = new GraphQLObjectType({
       },
       resolve(_, args) {
         return AuthService.signup(args);
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        username: {type: GraphQLString},
+        password: {type: GraphQLString}
+      },
+      resolve(_, args) {
+        return AuthService.login(args);
+      }
+    },
+    logout: {
+      type: UserType,
+      args: {
+        _id: { type: GraphQLID }
+      },
+      resolve(_,args){
+        return AuthService.logout(args);
+      }
+    },
+    verifyUser: {
+      type: UserType,
+      args: {
+        token: { type: GraphQLString }
+      },
+      resolve(_,args){
+        return AuthService.verifyUser(args);
       }
     }
   }
--- server/validation/login.js
+++ server/validation/login.js
@@ -2,8 +2,8 @@ const Validator = require("validator");
 const validText = require("./valid-text");
 
 module.exports = function validateLoginInput(data) {
-  data.username = ValidText(data.username) ? data.username : "";
-  data.password = ValidText(data.password) ? data.password : "";
+  data.username = validText(data.username) ? data.username : "";
+  data.password = validText(data.password) ? data.password : "";
 
   if (Validator.isEmpty(data.username)) {
     return { message: "Username field is required", isValid: false};
