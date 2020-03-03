8d3aaced8d208751c5fd0ac18d007887fb07e299
Kieran Scannell <kierscan@gmail.com>
2020-03-03 09:26:29 -0600

    Add file upload to s3 bucket

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const db = require("../config/keys").MONGO_URI;
+const keys = require("../config/keys");
+const db = keys.MONGO_URI;
 const expressGraphQL = require("express-graphql");
 const Models = require("./models/index.js");
 const schema = require("./schema/schema");
 const cors = require("cors");
+
+// used for aws s3 files
+const multer = require("multer");
+// const { graphqlUploadExpress } = require("graphql-upload");
+const { singleFileUpload } = require("./services/s3")
 
 const app = express();
 
@@ -18,15 +24,36 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-// remember we use bodyParser to parse requests into json
 app.use(bodyParser.json());
+
 app.use(cors());
+
 app.use(
   "/graphql",
+  // graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
   expressGraphQL({
     schema,
     graphiql: true
   })
 );
+
+// // below is used to configure uploading to aws buckets
+
+// configuring the DiscStorage engine.
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+//POST method route for uploading file
+app.post('/upload', upload.single("file"), function (req, res) {
+  //Multer middleware adds file(in case of single file ) or files(multiple files) object to the request object.
+  //req.file is the demo_file
+  singleFileUpload(req.file.path, req.file.filename, res);
+  // return res.send({ key: singleFileUpload(req.file) });
+});
 
 module.exports = app;
