e09d8c7aad822a73363d01f1c8102a5477e4b3c3
max821023 <max821023@gmail.com>
2020-03-03 08:50:03 -0800

    Add undo redo

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -26,7 +26,12 @@ class Nav extends React.Component {
                     <Sidebar />
                     <div className="header-wrapper">
                       <div className="header">
-                        <div className="header-left">back forward</div>
+                        <div className="header-left">
+                          <div className="undoredo">
+                            <i class="fas fa-undo"></i>
+                            <i class="fas fa-redo"></i>
+                          </div>
+                        </div>
                         <div className="header-right">
                           <h1>{data.username}</h1>
                           <button
