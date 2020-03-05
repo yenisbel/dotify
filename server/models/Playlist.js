2d7f50837f2eb140bb53892df1a97b4e3d6429e9
max821023 <max821023@gmail.com>
2020-03-05 09:55:05 -0800

    Update playlist schema

--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const PlaylistSchema = new Schema({
-  
-})
+  name: {
+    type: String,
+    required: true
+  },
+  creator: [{
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  }],
+  songs: [{
+    type: Schema.Types.ObjectId,
+    ref: "songs"
+  }]
+});
+
+module.exports = mongoose.model("playlists", PlaylistSchema);
