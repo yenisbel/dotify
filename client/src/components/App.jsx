37a08d832027104b57da25307cdc6ed7b2d7edd4
max821023 <max821023@gmail.com>
2020-03-05 12:44:12 -0800

    Commit before main index feed merge

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,11 +13,11 @@ const App = () => {
   return (
     <div>
       <Switch>
-        {/* <AuthRoute exact path="/login" component={Login} routeType="auth" />
+        <AuthRoute exact path="/login" component={Login} routeType="auth" />
         <AuthRoute exact path="/signup" component={Signup} routeType="auth" />
         <AuthRoute exact path="/splash" component={Splash} routeType="auth" />
-        <AuthRoute path="/" component={Nav} /> */}
         <Route exact path="/player" component={Player}/>
+        <AuthRoute path="/" component={Nav} />
         <Route path="/album/:id" component={AlbumShow} />
       </Switch>
     </div>
